2bf36a30f6308828cf39a069acf918d00c30503b
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-06-18 19:34:04 -0300

    ajuste

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ ln -s $LOCAL$OPENSPLICEGit $OPENSPLICELOCAL'/HDE'
 
 # Alterando arquivo ospl.xml original para o distribuído pela ContextNet
 mv /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml.OLD
-cp cp dependences/ospl.xml /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml
+cp dependences/ospl.xml /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml
 
 # Copiando variáveis de ambiente
 cp dependences/opensplice.sh /etc/profile.d/opensplice.sh
