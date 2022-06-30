851514696d5b55897ff07de2c88da0a58ea227db
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-06-30 08:12:14 -0300

    reiniciando a cada 8 horas

--- OpenSplice/contextNet.sh
+++ OpenSplice/contextNet.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
-
-/usr/bin/java -jar /opt/Robotic-agentCloud/OpenSplice/contextnet-2.7-eSMA.jar 127.0.0.1 5500 OpenSplice &
+while true
+do
+	echo "Iniciando contexNet"
+	/usr/bin/java -jar /opt/Robotic-agentCloud/OpenSplice/contextnet-2.7-eSMA.jar 127.0.0.1 5500 OpenSplice &
+	pid=$!
+	echo $pid
+	sleep 28800
+	echo "Encerrando contexNet"
+	kill $pid
+done
